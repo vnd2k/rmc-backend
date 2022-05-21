e11c4c9cfee06c99ce604f594425ae473668391e
Van Ngoc Dat <vndat74@gmail.com>
2022-05-21 09:50:18 +0700

    test postman

--- src/main/java/rmc/backend/rmc/RmcApplication.java
+++ src/main/java/rmc/backend/rmc/RmcApplication.java
@@ -3,13 +3,14 @@ package rmc.backend.rmc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class RmcApplication {
-    @RequestMapping("/")
+    @GetMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
